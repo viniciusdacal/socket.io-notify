3add6c370636393071475802a1b89c534e7e0908
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2016-03-18 13:26:21 -0300

    Call home

--- public/main.js
+++ public/main.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-  var socket = io.connect('127.0.0.1:3000', {
+  var socket = io.connect(window.location.hostname, {
     query: 'notificationKey=NOTIFICATION_KEY'
   });
   socket.on('NEW_NOTIFICATION', function (notification) {
